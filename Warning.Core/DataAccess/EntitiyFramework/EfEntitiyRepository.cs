8f5e72f271f31fdaaf99bbe88b1a290e9299ee94
Vacip <vacipderici@gmail.com>
2021-03-18 22:17:25 +0300

    Created Repository Interface

---
+++ Warning.Core/DataAccess/EntitiyFramework/EfEntitiyRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Warning.Core.Entities;
+
+eusing System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Warning.Core.DataAccess.EntitiyFramework
+{
+    public class EfEntitiyRepository<TEntity, TContext> : IEntityRepository<TEntity>
+         where TEntity : class, IEntity, new()
+        where TContext : DbContext, new()
+    {
+        public void Add(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity get(Expression<Func<TEntity, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TEntity> GetList(Expression<Func<TEntity, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
